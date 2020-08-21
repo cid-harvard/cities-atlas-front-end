3b6ff476370ceed5f7961c8f22956cd24936f656
Kyle Soeltz <wsoeltz@gmail.com>
2020-08-21 17:26:35 -0400

    Add panel search and city shapes to landing page

---
+++ src/components/map/MapSettingsComponent.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+export interface Settings {
+  allowZoom?: boolean;
+  allowPan?: boolean;
+  mapCallback?: (map: any) => void;
+}
+
+interface Props extends Settings{
+  map: any;
+}
+
+
+const MapSettingsComponent = (props: Props) => {
+  const {
+    map, mapCallback,
+  } = props;
+
+  const [haveSettingsBeenSet, setSettings] = useState<boolean>(false);
+
+  if (map && haveSettingsBeenSet === false) {
+    if (mapCallback !== undefined) {
+      mapCallback(map);
+    }
+    setSettings(true);
+  }
+
+  return <React.Fragment />;
+};
+
+export default MapSettingsComponent;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,8 +1,81 @@
 import React from 'react';
+import ClusterMap from '../components/map/ClusterLandingMap';
+import styled from 'styled-components/macro';
+import raw from 'raw.macro';
+import {CitiesGeoJsonData} from '../data/citiesTypes';
+import PanelSearch, {Datum as SearchDatum} from 'react-panel-search';
+import { Layer, Feature } from 'react-mapbox-gl';
+
+const geoJsonData: CitiesGeoJsonData = JSON.parse(raw('../data/cities.json'));
+
+const searchData: SearchDatum[] = [];
+
+const features: React.ReactElement<any>[] = [];
+
+geoJsonData.features.forEach(({properties, geometry: {coordinates}}, i) => {
+  const {
+    CTR_MN_NM: countryName, CTR_MN_ISO: parent_id, UC_NM_MN: title,
+    UC_NM_LST, AREA,
+  } = properties;
+  if (!searchData.find(d => d.id === parent_id)) {
+    searchData.push({
+      id: parent_id,
+      title: countryName,
+      parent_id: null,
+      level: '0',
+    });
+  }
+  const id = parent_id + '-' + title + '-' + UC_NM_LST + '-' + AREA + '-' + i;
+  searchData.push({
+    id,
+    title,
+    parent_id,
+    level: '1',
+  });
+  features.push(
+    <Feature
+      coordinates={coordinates[0]}
+      key={'geojson-' + id}
+    />,
+  );
+});
+
+const Root = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+`;
+
+const SearchContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
 
 const Landing = () => {
   return (
-    <h1>Landing Page</h1>
+    <Root>
+      <ClusterMap
+        zoom={[1.4]}
+      >
+        <Layer
+          type='fill'
+          id={'primary-map-geojson-layer'}
+          paint={{
+            'fill-color': '#F89570',
+          }}
+        >
+          {features}
+        </Layer>
+      </ClusterMap>
+      <SearchContainer>
+        <PanelSearch
+          data={searchData}
+          topLevelTitle={'Countries'}
+          disallowSelectionLevels={['0']}
+        />
+      </SearchContainer>
+    </Root>
   );
 };
 
