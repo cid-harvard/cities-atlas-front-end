886541c484ad9060b5c55e0429264e5ab4e162ba
Kyle Soeltz <wsoeltz@gmail.com>
2021-03-23 12:20:55 -0400

    Add bolding to search results

--- src/styling/utils/panelSearchKeywordFormatter.tsx
+++ src/styling/utils/panelSearchKeywordFormatter.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import Tooltip, {TooltipTheme} from '../../components/general/Tooltip';
-import {secondaryFont} from '../styleUtils';
+import {secondaryFont, baseColor} from '../styleUtils';
 import useFluent from '../../hooks/useFluent';
 
-const Root = styled.small`
-  opacity: 0.5;
+const RootDark = styled.small`
+  color: rgba(255, 255, 255, 0.5);
   width: 100%;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   display: block;
+
+  .panel-search-matched-keyword {
+    color: #fff;
+  }
+`;
+
+const RootLight = styled.small`
+  color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: block;
+
+  .panel-search-matched-keyword {
+    color: ${baseColor};
+  }
 `;
 
 const KeywordText = styled.div`
@@ -18,26 +35,29 @@ const KeywordText = styled.div`
 `;
 
 
-const KeywordElement = ({keywords, theme}: {keywords: string, theme: TooltipTheme}) => {
+const KeywordElement = ({keywords, theme, query}: {keywords: string, theme: TooltipTheme, query: string}) => {
+  const safeQuery = new RegExp(query.replace(/[^\w\s]/gi, '').trim(), 'gi');
   const getString = useFluent();
   const text = getString('global-ui-includes-keywords', {keywords});
+  const __html = getString('global-ui-includes-keywords', {keywords})
+    .replace(safeQuery, (match: string) => `<strong class='panel-search-matched-keyword'>${match}</strong>`);
+  const Root = theme === TooltipTheme.Dark ? RootDark : RootLight;
   return (
     <Tooltip
       explanation={<KeywordText>{text}</KeywordText>}
       delay={500}
       theme={theme}
     >
-      <Root>
-        {text}
-      </Root>
+      <Root dangerouslySetInnerHTML={{__html}} />
     </Tooltip>
   );
 };
 
-export default (theme: TooltipTheme) => (match: string[], rest: string[]) => {
-  if (match.length > 1 || rest.length > 1) {
+export default (theme: TooltipTheme) => (match: string[], rest: string[], query: string) => {
+  const total = match.length + rest.length;
+  if (!(match.length === total && rest.length === 0) && !(rest.length === total && match.length === 0)) {
     const keywords = [...match, ...rest].join(', ');
-    return <KeywordElement keywords={keywords} theme={theme} />;
+    return <KeywordElement keywords={keywords} theme={theme} query={query} />;
   } else {
     return null;
   }
