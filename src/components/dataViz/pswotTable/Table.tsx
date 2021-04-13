546030bdc8bca469d2e0b24460f9a8cc34edb57a
Kyle Soeltz <wsoeltz@gmail.com>
2021-04-13 11:33:30 -0400

    Add sorting to swot table

--- src/components/dataViz/pswotTable/Table.tsx
+++ src/components/dataViz/pswotTable/Table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   CompositionType,
 } from '../../../types/graphQL/graphQLTypes';
@@ -11,6 +11,10 @@ import {
   lightBorderColor,
   backgroundMedium,
 } from '../../../styling/styleUtils';
+import raw from 'raw.macro';
+import orderBy from 'lodash/orderBy';
+
+const sortArrows = raw('../../../assets/icons/sort-arrows.svg');
 
 const Root = styled.div`
   position: relative;
@@ -30,17 +34,19 @@ const ScrollRoot = styled(ScrollContainer)`
 
 const Table = styled.table`
   border-collapse: collapse;
+  min-width: 100%;
+  height: 100%;
 `;
 
 const Th = styled.th`
   position: sticky;
   top: 0;
   z-index: 20;
-  padding: 0.2rem 0.5rem;
   background-color: ${backgroundMedium};
   font-weight: 600;
   font-size: 0.8rem;
   text-transform: uppercase;
+  height: 100%;
 
   &:before {
     content: '';
@@ -70,12 +76,77 @@ const NameTh = styled(Th)`
   }
 `;
 
+const CellContent = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const SortContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  flex-grow: 1;
+`;
+
+const FilterContent = styled.div`
+  margin-top: 0.25rem;
+`;
+
+const activeClassBase = 'active-sort-column-';
+
+enum SortDirection {
+  ascending = 'asc',
+  descending = 'desc',
+}
+
+const SortArrowsBase = styled.div`
+  width: 0.5rem;
+  height: 0.8rem;
+  flex-shrink: 0;
+  margin-left: 0.5rem;
+
+  svg {
+    width: 100%;
+
+    polygon {
+      fill: #b9b9b9;
+    }
+  }
+
+  &.${activeClassBase + SortDirection.ascending} {
+    svg {
+      polygon.sort-arrow-up {
+        fill: #333;
+      }
+    }
+  }
+
+  &.${activeClassBase + SortDirection.descending} {
+    svg {
+      polygon.sort-arrow-down {
+        fill: #333;
+      }
+    }
+  }
+`;
+
 enum Quadrant {
   Potential = 'Potential',
   Strength = 'Strength',
   Weakness = 'Weakness',
   Opportunity = 'Opportunity',
   Threat = 'Threat',
+}
+
+enum SortField {
+  name = 'name',
+  rca = 'rca',
+  density = 'density',
+  quadrant = 'quadrant',
 }
 
 export const getQuadrant = (rca: number, density: number): Quadrant => {
@@ -107,6 +178,18 @@ const PSWOTTable = (props: Props) => {
   const {
     loading, error, data,
   } = props;
+
+  const [sortField, setSortField] = useState<SortField>(SortField.name);
+  const [sortDirection, setSortDirection] = useState<SortDirection>(SortDirection.ascending);
+
+  const toggleSort = (field: SortField) => {
+    if (sortField === field) {
+      setSortDirection(c => c === SortDirection.ascending ? SortDirection.descending : SortDirection.ascending);
+    } else {
+      setSortField(field);
+    }
+  };
+
   let output: React.ReactElement<any> | null;
   if (loading) {
     output = <LoadingBlock />;
@@ -118,7 +201,7 @@ const PSWOTTable = (props: Props) => {
     );
     console.error(error);
   } else if (data !== undefined) {
-    const rows = data.map(d => {
+    const rows = orderBy(data, [sortField], [sortDirection]).map(d => {
         return (
           <TableRow
             key={'table-row-' + d.id}
@@ -132,14 +215,76 @@ const PSWOTTable = (props: Props) => {
         );
       });
 
+    const activeClass = activeClassBase + sortDirection;
+
     output = (
       <Table>
         <thead>
           <tr>
-            <NameTh>Name</NameTh>
-            <Th>Relative Advantage</Th>
-            <Th>Predicted Growth</Th>
-            <Th>Assigned category</Th>
+            <NameTh>
+              <CellContent>
+                <SortContent
+                  onClick={() => toggleSort(SortField.name)}
+                >
+                  <div>Name</div>
+                  <SortArrowsBase
+                    className={sortField === SortField.name ? activeClass : undefined}
+                    dangerouslySetInnerHTML={{__html: sortArrows}}
+                  />
+                </SortContent>
+                <FilterContent>
+                  <input />
+                </FilterContent>
+              </CellContent>
+            </NameTh>
+            <Th>
+              <CellContent>
+                <SortContent
+                  onClick={() => toggleSort(SortField.rca)}
+                >
+                  <div>Relative Advantage</div>
+                  <SortArrowsBase
+                    className={sortField === SortField.rca ? activeClass : undefined}
+                    dangerouslySetInnerHTML={{__html: sortArrows}}
+                  />
+                </SortContent>
+                <FilterContent>
+                  <input />
+                </FilterContent>
+              </CellContent>
+            </Th>
+            <Th>
+              <CellContent>
+                <SortContent
+                  onClick={() => toggleSort(SortField.density)}
+                >
+                  <div>Predicted Growth</div>
+                  <SortArrowsBase
+                    className={sortField === SortField.density ? activeClass : undefined}
+                    dangerouslySetInnerHTML={{__html: sortArrows}}
+                  />
+                </SortContent>
+                <FilterContent>
+                  <input />
+                </FilterContent>
+              </CellContent>
+            </Th>
+            <Th>
+              <CellContent>
+                <SortContent
+                  onClick={() => toggleSort(SortField.quadrant)}
+                >
+                  <div>Assigned category</div>
+                  <SortArrowsBase
+                    className={sortField === SortField.quadrant ? activeClass : undefined}
+                    dangerouslySetInnerHTML={{__html: sortArrows}}
+                  />
+                </SortContent>
+                <FilterContent>
+                  <input />
+                </FilterContent>
+              </CellContent>
+            </Th>
           </tr>
         </thead>
         <tbody>
@@ -154,7 +299,10 @@ const PSWOTTable = (props: Props) => {
 
   return (
     <Root>
-      <ScrollRoot hideScrollbars={false}>
+      <ScrollRoot
+        hideScrollbars={false}
+        ignoreElements={'input'}
+      >
         {output}
       </ScrollRoot>
     </Root>
