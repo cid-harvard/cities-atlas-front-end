7f126fe05ff6c4f12d0f3f3790824ddc9b5c7ac2
Kyle Soeltz <wsoeltz@gmail.com>
2021-12-14 11:56:03 -0500

    Fix overview calling invalid city query

---
+++ src/pages/cities/profiles/overview/widgets/DisclaimerText.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { usePeerGroupCityCount } from '../../../../../components/navigation/secondaryHeader/comparisons/AddComparisonModal';
+import useFluent from '../../../../../hooks/useFluent';
+import useGlobalLocationData from '../../../../../hooks/useGlobalLocationData';
+
+
+const Root = styled.small`
+  text-align: center;
+  font-size: 0.65rem;
+  width: 100%;
+  display: block;
+`;
+
+interface Props {
+  cityId: string;
+  regionId: number | null;
+}
+
+const DisclaimerText = ({cityId, regionId}: Props) => {
+  const getString = useFluent();
+  const { data } = usePeerGroupCityCount(cityId);
+  const globalLocations = useGlobalLocationData();
+
+  let cityPeerGroupCountsRegion: string | number = '---';
+  let regionName: string = '---';
+  if (data && globalLocations.data) {
+    const region = globalLocations.data.regions.find(d => d.regionId === regionId + '');
+    regionName = region && region.regionName ? region.regionName : '';
+    cityPeerGroupCountsRegion = data.cityPeerGroupCounts.region;
+  }
+
+  return (
+    <Root>
+      <div>*{getString('city-overview-ranking-disclaimer', {
+          'city-peer-group-counts-region': cityPeerGroupCountsRegion,
+          'region-name': regionName,
+        })}
+      </div>
+      <div>**{getString('city-overview-benchmark-disclaimer')}</div>
+    </Root>
+  );
+};
+
+export default DisclaimerText;
--- src/pages/cities/profiles/overview/widgets/index.tsx
+++ src/pages/cities/profiles/overview/widgets/index.tsx
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 import { breakPoints } from '../../../../../styling/GlobalGrid';
 import TopRow from './topRow';
 import BottomRow from './bottomRow';
-import useFluent from '../../../../../hooks/useFluent';
-import { usePeerGroupCityCount } from '../../../../../components/navigation/secondaryHeader/comparisons/AddComparisonModal';
-import useGlobalLocationData from '../../../../../hooks/useGlobalLocationData';
 import useCurrentCity from '../../../../../hooks/useCurrentCity';
+import DisclaimerText from './DisclaimerText';
 
 const Root = styled.div`
   width: 100%;
@@ -30,26 +28,15 @@ const Row = styled.div`
   }
 `;
 
-const DisclaimerText = styled.small`
-  text-align: center;
-  font-size: 0.65rem;
-  width: 100%;
-  display: block;
-`;
 
 const Widgets = () => {
-  const getString = useFluent();
   const { city } = useCurrentCity();
-  const { data } = usePeerGroupCityCount(city && city.cityId ? city.cityId : null);
-  const globalLocations = useGlobalLocationData();
-
-  let cityPeerGroupCountsRegion: string | number = '---';
-  let regionName: string = '---';
-  if (data && globalLocations.data && city) {
-    const region = globalLocations.data.regions.find(d => d.regionId === city.region + '');
-    regionName = region && region.regionName ? region.regionName : '';
-    cityPeerGroupCountsRegion = data.cityPeerGroupCounts.region;
-  }
+  const disclaimer = city ? (
+    <DisclaimerText
+      cityId={city.cityId}
+      regionId={city.region}
+    />
+  ) : null;
   return (
     <Root>
       <Row>
@@ -59,14 +46,7 @@ const Widgets = () => {
         <BottomRow />
       </Row>
       <Row>
-        <DisclaimerText>
-          <div>*{getString('city-overview-ranking-disclaimer', {
-              'city-peer-group-counts-region': cityPeerGroupCountsRegion,
-              'region-name': regionName,
-            })}
-          </div>
-          <div>**{getString('city-overview-benchmark-disclaimer')}</div>
-        </DisclaimerText>
+        {disclaimer}
       </Row>
     </Root>
   );
