ce70691ec8a27df432315adac54f434c205076e1
steven-geofrey <s.geofrey@fluidencodings.com>
2022-06-15 12:10:43 -0400

    Fixed name of Sentry environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -13,11 +13,11 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
-if(process.env.SENTRY_DSN && process.env.SENTRY_ENV) {
+if(process.env.REACT_APP_SENTRY_DSN && process.env.REACT_APP_SENTRY_ENV) {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.REACT_APP_SENTRY_DSN,
     integrations: [new BrowserTracing()],
-    environment: process.env.SENTRY_ENV,
+    environment: process.env.REACT_APP_SENTRY_ENV,
   
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
