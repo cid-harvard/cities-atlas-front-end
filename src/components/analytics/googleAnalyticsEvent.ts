f8ad50f40907c03627ac34ff4c6a913a62b4c2e5
Steven Geofrey <s.geofrey@fluidencodings.com>
2022-09-13 09:06:05 -0400

    Use ReactGA.event() method for GA4 events

--- src/components/analytics/googleAnalyticsEvent.ts
+++ src/components/analytics/googleAnalyticsEvent.ts
@@ -4,7 +4,8 @@ const googleAnalyticsEvent = (category: string, action: string, label: string) =
   if (process.env.NODE_ENV === 'production') {
     // See: https://developers.google.com/analytics/devguides/collection/gtagjs/events
     // See: https://www.npmjs.com/package/react-ga4
-    ReactGA.send({hitType: "event", category: category, label: label, action: action});
+    // ReactGA.send({hitType: "event", category: category, label: label, action: action});
+    ReactGA.event({category: category, label: label, action: action});
 
   }
 };
