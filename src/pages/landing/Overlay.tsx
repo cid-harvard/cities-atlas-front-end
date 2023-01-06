68782806e23f8225da40e32b262dc973a5295cbf
Steven Geofrey <s.geofrey@fluidencodings.com>
2023-01-06 14:51:43 -0500

    Vertically center landing page contents with IIBA banner

--- src/pages/landing/Overlay.tsx
+++ src/pages/landing/Overlay.tsx
@@ -25,7 +25,7 @@ const Root = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: center;
   pointer-events: none;
   z-index: 100;
 
